e8446bcd1afafbad1c6cf1f07cb4eb80a1204208
Peh CX <57000214+pehcx@users.noreply.github.com>
2024-11-03 17:55:14 +0800

    Set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgHcaptchaModule } from 'ng-hcaptcha';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgHcaptchaModule.forRoot({
+      siteKey: environment.hcaptcha_sitekey,
+      languageCode: 'en' // optional, will default to browser language
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
