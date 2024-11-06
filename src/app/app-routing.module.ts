9282c288b2f3776f303dbde49f086e64e2a76d01
Peh CX <57000214+pehcx@users.noreply.github.com>
2024-11-07 01:55:44 +0800

    components update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'aboutus', component: AboutusComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -8,17 +8,26 @@ import { NgHcaptchaModule } from 'ng-hcaptcha';
 import { environment } from 'src/environments/environment';
 import { IconComponent } from './components/icon/icon.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { SplitPipe } from './pipes/split.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     IconComponent,
+    HomeComponent,
+    ProductsComponent,
+    AboutusComponent,
+    SplitPipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     // NgHcaptchaModule.forRoot({
     //   siteKey: environment.hcaptcha_sitekey,
     //   languageCode: 'en' // optional, will default to browser language
