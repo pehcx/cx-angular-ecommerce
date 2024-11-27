a27fe00b1be6ac9fa2000f6c80e2c9068293d781
Peh CX <57000214+pehcx@users.noreply.github.com>
2024-11-28 01:58:04 +0800

    update getSession logic

--- src/app/core/services/supabase.service.ts
+++ src/app/core/services/supabase.service.ts
@@ -2,7 +2,6 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { createClient, Session, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from 'src/environments/environment';
 import { AuthState } from '../enums/auth-state';
-import { ErrorHandlerService } from './error-handler.service';
 import { QueryParams } from '../interfaces/query-params.interface';
 
 @Injectable({
@@ -11,13 +10,10 @@ import { QueryParams } from '../interfaces/query-params.interface';
 
 export class SupabaseService {
   private supabase: SupabaseClient;
-  private cachedSession: Session | null = null;
-  private user: any = null;
+  private session: Session | null = null;
   authStateChanged: EventEmitter<AuthState> = new EventEmitter<AuthState>();
 
-  constructor(
-    private errorHandlerService: ErrorHandlerService
-  ) {
+  constructor() {
     this.supabase = createClient(environment.supabase_url, environment.supabase_key, {
       auth: {
         // This is the default config
@@ -84,46 +80,57 @@ export class SupabaseService {
   }
 
   public async signOut() {
-    const { error } = await this.supabase.auth.signOut()
-    if (error) {
-      this.errorHandlerService.sendError(error);
-    }
+    const { error } = await this.supabase.auth.signOut();
+
+    if (error) throw error;
   }
 
   public getSession(): Session | null {
-    return this.cachedSession;
+    return this.session;
   }
 
-  public getUser() {
-    return this.user;
-  }
+  // public async retrieveSession() {
+  //   try {
+  //     const { data, error } = await this.supabase.auth.getSession();
+
+  //     if (error) throw error;
+      
+  //     return {
+  //       data: data,
+  //       error: null
+  //     };
+  //   } catch (error) {
+  //     return {
+  //       data: null,
+  //       error: error
+  //     };
+  //   }
+  // }
 
   private setupAuthStateChangeHandler() {
     this.supabase.auth.onAuthStateChange(async (event, session) => {
       switch (event) {
         case 'SIGNED_IN':
-          this.cachedSession = session;
-          this.user = session?.user ?? null;
+          this.session = session;
           this.authStateChanged.emit(AuthState.SIGNED_IN);
           break;
 
         case 'SIGNED_OUT':
-          this.cachedSession = null;
+          this.session = null;
           this.authStateChanged.emit(AuthState.SIGNED_OUT);
           break;
 
         case 'TOKEN_REFRESHED':
-          this.cachedSession = session;
+          this.session = session;
           this.authStateChanged.emit(AuthState.TOKEN_REFRESHED);
           break;
 
         case 'USER_UPDATED':
-          this.fetchUserData();
+          // this.supabase.auth.refreshSession();
           break;
 
         default:
           if (!Object.values(AuthState).includes(event as AuthState)) {
-            this.errorHandlerService.sendError();
             console.warn('Encountered an unhandled Supabase auth state change event', event);
           }
           break;
@@ -139,27 +146,8 @@ export class SupabaseService {
       .order(queries.orderBy, { ascending: queries.sortByAsc })
       .limit(queries.limitBy);
 
-    if (error) console.error(error);
+    if (error) throw error;
     return data ?? [];
-  }
-
-  /**
-   * Fetches the current user's data from Supabase and updates the internal user state.
-   * Emits the `USER_UPDATED` event to notify subscribers once the user data has been successfully fetched.
-   *
-   * This method should only be called by `setupAuthStateChangeHandler` function
-   * to ensure it is invoked during relevant auth state transitions.
-   *
-   * @returns {Promise<void>}
-   */
-  private async fetchUserData() {
-    const { data, error } = await this.supabase.auth.getUser();
-    if (data) {
-      this.user = data;
-      this.authStateChanged.emit(AuthState.USER_UPDATED);
-    }
-    
-    if (error) console.error("Failed to retrieve user data");
   }
 
   private buildQueries(queryParams: QueryParams = {}) {
